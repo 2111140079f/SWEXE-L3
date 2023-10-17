60354e6bd88205d21f3b583e36d3a5a1c1bc14b8
Yuya Yamasaki <ubuntu@ip-172-31-30-196.ec2.internal>
2023-10-17 06:18:09 +0000

    push to render.com

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "adaece2c47724868a399abbda26ea743.vfs.cloud9.us-east-1.amazonaws.com"
 end
